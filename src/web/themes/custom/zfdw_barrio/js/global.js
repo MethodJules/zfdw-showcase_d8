d1726e1e2c4d89a0583e035ee75ebf3fe43f5055
MethodJules <hoferj@uni-hildesheim.de>
2020-01-27 15:37:44 +0100

    started zfdw barrio theme css

---
+++ src/web/themes/custom/zfdw_barrio/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.zfdw_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
